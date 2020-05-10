827ae82f7a16dd07e5dd4b540daf7a0df42f28e4
Michele Contu <thesorcerer13@gmail.com>
2020-05-10 17:39:10 +0200

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -97,9 +97,7 @@ export const IndexPageTemplate = ({
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
+                   
                   </div>
                 </div>
               </div>
